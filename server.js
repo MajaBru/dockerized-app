bbe662f37fa015b653ec52c9784b3e112bf64f80
Maja <122542734+MajaBru@users.noreply.github.com>
2024-02-06 14:57:34 +0100

    port

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const mongo = require("mongodb");
 const path = require("path");
-const config = require("./config.json");
+
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+console.log(`Server is up on port: ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('hi');
+});
